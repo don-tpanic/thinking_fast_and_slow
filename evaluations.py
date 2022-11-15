c2030402c1033b5ec5f1ed486996c9a50782175c
Xiaoliang Luo <ken.xiaoliang@gmail.com>
2022-11-15 14:59:16 +0000

    add code: eval code now works for fast, slow and total model results

---
+++ evaluations.py
@@ -0,0 +1,118 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from collections import OrderedDict
+import torch
+
+import models
+from utils import load_config
+plt.rcParams.update({'font.size': 8})
+
+
+def examine_lc(config_version, 
+                problem_types=[1, 2, 3, 4, 5, 6]):
+    """
+    Follow sustain impl, we examine learning curves (y-axis is proberror)
+
+    return:
+    -------
+        trapz_areas: An array of scalars which are the areas under the learning curves computed
+                     using the trapzoidal rule.
+        figure (optional): If `plot_learn_curves=True` will plot the learning curves.
+    """
+    config = load_config(config_version)
+    num_runs = config['num_runs']
+    num_blocks = config['num_blocks']
+    model_types = ['total', 'fast', 'slow']
+    colors = ['blue', 'orange', 'black', 'green', 'red', 'cyan']
+    fig, axes = plt.subplots(3, 1)
+
+    for subplot_idx in range(len(model_types)):
+        model_type = model_types[subplot_idx]
+
+        for idx in range(len(problem_types)):
+            problem_type = problem_types[idx]
+
+            lc_file = f'results/{config_version}/lc_{model_type}_type{problem_type}.npy'
+            lc = np.load(lc_file)[:num_blocks]
+
+            axes[subplot_idx].errorbar(
+                range(lc.shape[0]), 
+                lc, 
+                color=colors[idx],
+                label=f'Type {problem_type}',
+            )
+            axes[subplot_idx].set_title(f'{model_type}')
+            axes[subplot_idx].set_ylim(0, 0.7)
+            axes[subplot_idx].set_ylabel('proberror')
+            axes[-1].set_xlabel('epochs')
+    
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(f'results/{config_version}/lc.png')
+    plt.close()
+            
+            
+def examine_recruited_clusters_n_attn(config_version, canonical_runs_only=True):
+    """
+    Record the runs that produce canonical solutions
+    for each problem type. 
+    Specificially, we check the saved `mask_non_recruit`
+    """
+    config = load_config(config_version)
+    num_runs=config['num_runs']
+    results_path = f'results/{config_version}'
+
+    num_types = 6
+    results_path = f'results/{config_version}'
+    num_runs = config['num_runs']
+    num_dims = 3
+    type2cluster = {
+        1: 2, 2: 4,
+        3: 6, 4: 6, 5: 6,
+        6: 8}
+
+    from collections import defaultdict
+    canonical_runs = defaultdict(list)
+
+    attn_weights = []
+    for z in range(num_types):
+        problem_type = z + 1
+        
+        all_runs_attn = np.empty((num_runs, num_dims))
+        for run in range(num_runs):            
+            model_path = os.path.join(results_path, f'type{problem_type}_run{run}.pth.tar')
+            model_state_dict = torch.load(model_path)['state_dict']
+            mask_non_recruit = model_state_dict['ClusteringModel.MaskNonRecruit.weight']
+            dim_wise_attn_weights = model_state_dict['ClusteringModel.DimensionWiseAttnLayer.weight']            
+            all_runs_attn[run, :] = dim_wise_attn_weights
+
+            num_nonzero = len(np.nonzero(mask_non_recruit.detach().numpy()[0])[0])
+            if canonical_runs_only:
+                if num_nonzero == type2cluster[problem_type]:
+                    canonical_runs[z].append(run)
+            else:
+                canonical_runs[z].append(run)
+
+        per_type_attn_weights = np.round(
+            np.mean(all_runs_attn, axis=0), 3
+        )
+        attn_weights.append(per_type_attn_weights)
+
+    proportions = []
+    for z in range(num_types):
+        print(f'Type {z+1}, has {len(canonical_runs[z])}/{num_runs} canonical solutions')
+        proportions.append(
+            np.round(
+                len(canonical_runs[z]) / num_runs 
+            )
+        )
+    
+    print(attn_weights)
+    return proportions, attn_weights
+
+
+if __name__ == '__main__':
+    examine_lc(config_version='config1')
+    examine_recruited_clusters_n_attn(config_version='config1')
