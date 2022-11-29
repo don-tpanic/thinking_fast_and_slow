5d40914f543ac3ba623d64de5a0ebd5ba637ba37
Xiaoliang Luo <ken.xiaoliang@gmail.com>
2022-11-29 14:47:14 +0000

    add code: option to use multiunit; nested config

--- models.py
+++ models.py
@@ -3,22 +3,24 @@ from torch import nn
 
 from clustering_model.models import ClusteringModel 
 from dnn_model.models import NeuralNetwork
+from multiunit_clustering.models import MultiUnitCluster
 
 
 class FastSlow(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        
+        self.config = config
+
         # initializing models
         if config['fast'] == 'clustering':
-            self.FastModel = ClusteringModel(config=config)
+            self.FastModel = ClusteringModel(config=config['fast_config'])
 
-        elif config['fast'] == 'local':
-            NotImplementedError()
+        elif config['fast'] == 'multiunit_clustering':
+            self.FastModel = MultiUnitCluster(config=config['fast_config'])
 
         if config['slow'] == 'dnn':
-            self.SlowModel = NeuralNetwork(config=config)
+            self.SlowModel = NeuralNetwork(config=config['slow_config'])
         
         # initializing optimizer and loss function
         # NOTE: model specific lr are handled internally in 
@@ -32,7 +34,16 @@ class FastSlow(nn.Module):
             self.loss_fn = nn.BCEWithLogitsLoss()
 
     def forward(self, inp, epoch, i, signature, y_true):
-        y_pred_fast = self.FastModel(inp, epoch, i, signature, y_true)
-        y_pred_slow = self.SlowModel(inp)
-        y_pred_total = torch.add(y_pred_fast, y_pred_slow)
-        return y_pred_fast, y_pred_slow, y_pred_total
+        if self.config['fast'] == 'clustering':
+            y_logits_fast = self.FastModel(inp, epoch, i, signature, y_true)
+            extra_stuff = ()
+
+        elif self.config['fast'] == 'multiunit_clustering':
+            y_logits_fast, act, win_ind = self.FastModel(inp, epoch, i, y_true)
+            extra_stuff = (act, win_ind)
+
+        y_logits_slow = self.SlowModel(inp)
+        y_logits_total = torch.add(y_logits_fast, y_logits_slow)
+
+        # TODO: better way handle extra outputs?
+        return y_logits_fast, y_logits_slow, y_logits_total, extra_stuff
